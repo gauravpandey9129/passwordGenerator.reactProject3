ee6e18c91f1a2e040fe6735503571793403e7e2b
gauravpandey9129 <gaurav912959@gmail.com>
2024-02-12 22:09:37 +0530

    AdeedFunctionWhichGeneratePassword

--- mp-app/src/App.js
+++ mp-app/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 
 
 function App() {
@@ -6,17 +6,44 @@ function App() {
   const [number,setNumber]=useState(false)
   const [characters,setCharacters]=useState(false)
   const [range,setRange]=useState(8)
-  return (
-  <> 
+
+  const passwordGenerator=useCallback(()=>{
+     let pass='';
+     let str='ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+     if(number) str+='1234567890';
+     if(characters) str+='!@#$&*(){}[]';
+     for (let i = 1; i <= range; i++) {
+      let randomNumber=Math.floor(Math.random() * str.length+1);
+      pass+=str[randomNumber]
       
-       <div className=" border-danger text-light bg-dark-subtle text-center w-50 mx-auto p-5">
+     }
+
+     setPassword(pass)
+
+  },[setPassword,characters,number,range])
+
+  useEffect(()=>{
+    passwordGenerator()
+  },[passwordGenerator])
+  return (
+  < > 
+      
+       <div className=" border-danger  bg-dark-subtle text-center w-50 mx-auto p-5">
        <div className="input-group mb-3">
   
-        <input type="text" className="form-control input-group-large" value={password} aria-label="Example text with button addon" aria-describedby="button-addon1"/>
+        <input type="text"   readOnly className="form-control input-group-large" value={password} aria-label="Example text with button addon" aria-describedby="button-addon1"/>
         <button className="btn btn-outline-secondary text-light btn-outline-warning  btn-primary" type="button" id="button-addon1">Copy</button>
          </div>
-         <label htmlFor="range">Number Of Words={range}
-         <input type="range"   value={range} onChange={(event)=>{ setRange(event.target.value)}}min={5} max={20} id="range"></input></label>
+         <div className="container bg-danger-subtle">
+         <input type="range"   value={range} onChange={(event)=>{ setRange(event.target.value)}}min={5} max={20} id="range"></input><br/>
+         <label     htmlFor="range">Select Length of Password = {range}</label></div>
+         <br/>
+         
+         <input type="checkbox"  id="number" onChange={()=>setNumber((prev)=>!prev)}></input>
+         <label htmlFor="number">Number</label><br/>
+         <input type="checkbox"  id="char" onChange={()=>setCharacters((prev)=>!prev)}></input>
+         <label htmlFor="char">Character</label>
+         
 
         </div>
        
